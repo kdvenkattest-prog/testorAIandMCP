043c666a1932a5b6bce7c7e23415dcc21844d2ad
kdvenkattest-prog <kdvenkattest@gmail.com>
2026-05-26 12:58:45 +0530

    update API key
    update API key

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import os
 # -----------------------------
 # BAD PRACTICE #1: Hardcoded secret
 # -----------------------------
-API_KEY = "12345-SECRET-KEY"   # ❌ Never hardcode secrets
+API_KEY = os.getenv("API_KEY")   # ❌ Never hardcode secrets
 
 
 # Better:

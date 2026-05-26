2e310043db1dcb77d7f8839d35b0791019d1121d
kdvenkattest-prog <kdvenkattest@gmail.com>
2026-05-26 16:35:23 +0530

    hard coded
    hard codedhard codedhard coded

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import os
 # -----------------------------
 # BAD PRACTICE #1: Hardcoded secret
 # -----------------------------
-API_KEY = os.getenv("API_KEY")   # ❌ Never hardcode secrets
+API_KEY = "123-123-123"   # ❌ Never hardcode secrets
 
 
 # Better:

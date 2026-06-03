5a5ced8e92194ed3a7927914638a4a78460fd4da
kdvenkattest-prog <kdvenkattest@gmail.com>
2026-06-03 14:27:15 +0530

    REMOVING hardcode secreat

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ from httpx import (
 # -----------------------------
 # BAD PRACTICE #1: Hardcoded secret
 # -----------------------------
-API_KEY = "12345-SECRET-KEY"   # ❌ Never hardcode secrets
+#API_KEY = "12345-SECRET-KEY"   # ❌ Never hardcode secrets
 * GitLab API: https://docs.gitlab.com/ce/api/branches.html
 
 # Better:
